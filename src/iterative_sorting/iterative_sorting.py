6d69a195706ebbaaf12ec8571f5bfc4aaaa7d5e9
Rayven <rayven.burns@gmail.com>
2020-02-17 18:03:12 -0500

    finished mvp day 1 and linear and binary search

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,27 +1,38 @@
-# TO-DO: Complete the selection_sort() function below 
+
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
 
+        for j in range(i+1, len(arr)): #have to iterate through the array (looping through elements ono right hand side of current index)
+            if arr[j] < arr[smallest_index]: #if current position is less than the smallest index
+                smallest_index = j #smallest index is the current position
 
-
-        # TO-DO: swap
-
-
-
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i] #swap element at current index with the smallest element found in above loop
 
     return arr
+arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+
+   
 
 
-# TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    swapped = True 
+
+    while swapped: #while swapped is True
+        swapped = False #set it to false at first(also no infinite loop)
+
+        for i in range(0, len(arr)-1): #loop through array
+
+            if arr[i] > arr[i + 1]: #if the current element is greater than one next to it
+            
+                arr[i], arr[i+1] = arr[i+1], arr[i] #swap them
+                swapped = True #swapping has happened, so true
 
     return arr
+print(bubble_sort(arr1))
+
 
 
 # STRETCH: implement the Count Sort function below
